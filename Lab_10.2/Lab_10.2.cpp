56fa6d1324ab3a4eab4eb0f215796d72d3d6a4e7
oleksandr.luchkevych.ri.2022@lpnu.ua <oleksandr.luchkevych.ri.2022@lpnu.ua>
2022-12-22 13:37:18 +0200

    Add project files.

---
+++ Lab_10.2/Lab_10.2.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+string Change(string& stringToCount)
+{
+	int i = 0;
+	int len = stringToCount.length();
+	size_t pos = 0;
+
+	while ((pos = stringToCount.find('.', pos)) < (len / 1) * 1 - 1)
+	{
+		stringToCount[pos + 1] = toupper(stringToCount[pos + 1]);
+		pos++;
+	}
+	return stringToCount;
+}
+
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    ifstream file("t.txt");
+
+    string stringToCount;
+
+    while (getline(file, stringToCount))//поки можна зчитати файл
+        file >> stringToCount;//зчитування з файлу 
+
+    ofstream file2("t2.txt");
+	file2 << Change(stringToCount);
+	file2.close();
+
+	cout << Change(stringToCount) << endl;
+	return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_10.2/Lab_10.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			string stingToCount{ "hi...kl" };
+			string t = Change(stingToCount);
+			string l{ "hi...Kl" };
+			Assert::AreEqual(t, l);
+		}
+	};
+}
